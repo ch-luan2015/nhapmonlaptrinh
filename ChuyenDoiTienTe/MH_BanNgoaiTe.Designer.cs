dd53717d2d181074c42bc13607b9c225855d9379
ch-luan2015 <ch.luan2015@gmail.com>
2021-12-17 01:19:14 +0700

    Finish

--- ChuyenDoiTienTe/MH_BanNgoaiTe.Designer.cs
+++ ChuyenDoiTienTe/MH_BanNgoaiTe.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Th_TygiaUSD = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Th_SoLuongUSDMua = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Th_DongYThanhToan = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Th_ThanhTien = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Th_TygiaUSD
+            // 
+            this.Th_TygiaUSD.AutoSize = true;
+            this.Th_TygiaUSD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Th_TygiaUSD.Location = new System.Drawing.Point(484, 129);
+            this.Th_TygiaUSD.Name = "Th_TygiaUSD";
+            this.Th_TygiaUSD.Size = new System.Drawing.Size(0, 25);
+            this.Th_TygiaUSD.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(268, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Số Lượng:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(266, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Tỷ giá USD hiện nay:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(148, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "USD";
+            // 
+            // Th_SoLuongUSDMua
+            // 
+            this.Th_SoLuongUSDMua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Th_SoLuongUSDMua.Location = new System.Drawing.Point(475, 208);
+            this.Th_SoLuongUSDMua.Name = "Th_SoLuongUSDMua";
+            this.Th_SoLuongUSDMua.Size = new System.Drawing.Size(204, 30);
+            this.Th_SoLuongUSDMua.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(265, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 32);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Bán Ngoại Tệ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Th_DongYThanhToan
+            // 
+            this.Th_DongYThanhToan.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Th_DongYThanhToan.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Th_DongYThanhToan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Th_DongYThanhToan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Th_DongYThanhToan.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Th_DongYThanhToan.Location = new System.Drawing.Point(271, 356);
+            this.Th_DongYThanhToan.Name = "Th_DongYThanhToan";
+            this.Th_DongYThanhToan.Size = new System.Drawing.Size(204, 66);
+            this.Th_DongYThanhToan.TabIndex = 12;
+            this.Th_DongYThanhToan.Text = "Thanh toán";
+            this.Th_DongYThanhToan.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(266, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Thành Tiền:";
+            // 
+            // Th_ThanhTien
+            // 
+            this.Th_ThanhTien.AutoSize = true;
+            this.Th_ThanhTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Th_ThanhTien.Location = new System.Drawing.Point(475, 279);
+            this.Th_ThanhTien.Name = "Th_ThanhTien";
+            this.Th_ThanhTien.Size = new System.Drawing.Size(0, 25);
+            this.Th_ThanhTien.TabIndex = 22;
+            // 
+            // MH_BanNgoaiTe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Th_ThanhTien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Th_TygiaUSD);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Th_SoLuongUSDMua);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Th_DongYThanhToan);
+            this.Name = "MH_BanNgoaiTe";
             this.Text = "MH_BanNgoaiTe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label Th_TygiaUSD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Th_SoLuongUSDMua;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Th_DongYThanhToan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Th_ThanhTien;
     }
 }
--- ChuyenDoiTienTe/MH_BanNgoaiTe.cs
+++ ChuyenDoiTienTe/MH_BanNgoaiTe.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ChuyenDoiTienTe
 {
@@ -15,6 +16,31 @@ namespace ChuyenDoiTienTe
         public MH_BanNgoaiTe()
         {
             InitializeComponent();
+            //Hien ty gia
+
+            var TyGiaUSD = int.Parse(File.ReadAllText("TyGiaUSD.txt"));
+
+            Th_TygiaUSD.Text = TyGiaUSD.ToString();
+            //Nhap so luong
+            int SoLuongMua = 0;
+
+            Th_DongYThanhToan.Click += (Th, Ts) =>
+            {
+                Boolean checkInput = int.TryParse(Th_SoLuongUSDMua.Text.Trim(), out SoLuongMua);
+
+                if (checkInput && Th_SoLuongUSDMua.Text.Trim()!="" && SoLuongMua>=1 )
+                {
+                    var ThanhTien = SoLuongMua * TyGiaUSD;
+                    Th_ThanhTien.Text = ThanhTien.ToString();
+                }
+                else
+                {
+                    Th_ThanhTien.Text = "Xin hãy kiểm tra lại số lượng nhập";
+                }
+            };
+           
+
         }
+
     }
 }
